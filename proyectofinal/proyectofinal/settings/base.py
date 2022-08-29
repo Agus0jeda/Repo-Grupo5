8e89cd5fa318e2b20f51c9f1c93c15298de3b422
Walter-Marcelo-Gonzalez <walterwalter1976@gmail.com>
2022-08-29 18:37:00 -0300

    comentando(cancelando)parte DATABASES en archivo base.py y agrego esta parte en local.py para verificar no suba

--- proyectofinal/proyectofinal/settings/base.py
+++ proyectofinal/proyectofinal/settings/base.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'proyectofinal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -84,7 +85,7 @@ DATABASES = {
         'PORT':'5432',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
