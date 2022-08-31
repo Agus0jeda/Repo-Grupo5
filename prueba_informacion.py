76a5e8fd416520bf3de70ba310009fdc871d7ef8
Caro_Huell <caro08serfaty@gmail.com>
2022-08-31 08:15:42 -0300

    .

---
+++ prueba_informacion.py
@@ -0,0 +1,17 @@
+
+</header>
+
+<section id="content" style="margin-bottom: 0px;">
+<div class="home-page">
+<section style="margin-bottom: 25px; margin-top: 40px" class="container common-height clearfix">
+<div class="heading-block home-heading-block">
+<h1>LALCEC</h1>
+</div>
+<div class="row clearfix">
+<div class="col-md-6" style="text-align: justify">
+<p> MISIÓN: <strong><a target="_blank" href=">LALCEC, 
+tiene como objetivo</a></strong> conseguir tratamiento para todos,
+alcanzar estilos de vida saludables,</p>
+<p>ofrecer servicios de detección precoz <strong>y sumar voluntades para concientizar </strong>a la población sobre <strong>
+la campaña de lucha contra el cáncer. </p>
+</p>
